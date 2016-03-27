cd1b9b290329375d9104a8b083eea2e12ffae43f
Kim SAVAROCHE <kimsavinfo@gmail.com>
2016-03-27 19:25:10 +0200

    gemme shapes OK

---
+++ OpenCV_triangleDetection/OpenCV_triangleDetection/AppDelegate.h
@@ -0,0 +1,18 @@
+//
+//  AppDelegate.h
+//  OpenCV_triangleDetection
+//
+//  Created by Kim SAVAROCHE on 26/03/2016.
+//  Copyright (c) 2016 Kim SAVAROCHE. All rights reserved.
+//
+
+
+#import <UIKit/UIKit.h>
+
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
